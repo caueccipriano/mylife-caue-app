cafff7870482b37538d7b3b9e76947221fa89729
caueccipriano <caueccipriano@gmail.com>
2026-09-19 16:53:37 -0300

    chore: bump EU v3 runtime version

--- src/main.tsx
+++ src/main.tsx
@@ -4,7 +4,7 @@ import { HashRouter } from 'react-router-dom'
 import App from './App'
 import './styles.css'
 
-const APP_VERSION = 'eu-v2-5-intelligence'
+const APP_VERSION = 'eu-v3-living-system'
 
 async function refreshPwaShell() {
   if (!('serviceWorker' in navigator)) return
