aac6e4aab73c4b13fb26ee74156232387ddc1a78
caueccipriano <caueccipriano@gmail.com>
2026-09-19 14:01:22 -0300

    feat: route astrology inside Vida

--- src/App.tsx
+++ src/App.tsx
@@ -11,7 +11,7 @@ import ChatInboxPage from './ChatInboxPage'
 import ReviewPage from './ReviewPage'
 import AskEuPage from './AskEuPage'
 import EntityPage from './EntityPage'
-import SnapshotsPage from './SnapshotsPage'
+import SnapshotsPage from './SnapshotsPage'\nimport AstrologyPage from './AstrologyPage'
 
 const nav = [
   { path: '/', label: 'Hoje', icon: '☀' },
@@ -39,7 +39,7 @@ function AppShell({ onRegister }: { onRegister: () => void }) {
           <Route path="/revisao" element={<ReviewPage />} />
           <Route path="/pergunte" element={<AskEuPage />} />
           <Route path="/assunto/:slug" element={<EntityPage />} />
-          <Route path="/vida/fases" element={<SnapshotsPage />} />
+          <Route path="/vida/fases" element={<SnapshotsPage />} />\n          <Route path="/vida/astrologia" element={<AstrologyPage />} />
 
           <Route path="/areas" element={<Navigate to="/vida" replace />} />
           <Route path="/projetos" element={<Navigate to="/vida" replace />} />
