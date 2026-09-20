da78ac97fc90b08e6b2f44112a3a59cb489dc3ec
caueccipriano <caueccipriano@gmail.com>
2026-09-20 10:44:36 -0300

    chore: bump EU Prism release

--- src/main.tsx
+++ src/main.tsx
@@ -4,7 +4,7 @@ import { HashRouter } from 'react-router-dom'
 import App from './App'
 import './styles.css'
 
-const APP_VERSION = 'eu-v3-cool-palette'
+const APP_VERSION = 'eu-v3-prism'
 
 async function refreshPwaShell() {
   if (!('serviceWorker' in navigator)) return
