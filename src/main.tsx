eef06c1a8ed694c1ea66dcd2a27afd308b235ffb
caueccipriano <caueccipriano@gmail.com>
2026-09-20 12:16:20 -0300

    chore: finalize EU v4 notification release

--- src/main.tsx
+++ src/main.tsx
@@ -4,7 +4,7 @@ import { HashRouter } from 'react-router-dom'
 import App from './App'
 import './styles.css'
 
-const APP_VERSION = 'eu-v4-clarity'
+const APP_VERSION = 'eu-v4-notifications-final'
 
 async function refreshPwaShell() {
   if (!('serviceWorker' in navigator)) return
