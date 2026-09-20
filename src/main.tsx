8525404efe26eb16762890a74cf1dff7ef428562
caueccipriano <caueccipriano@gmail.com>
2026-09-20 10:29:14 -0300

    chore: bump EU cool palette release

--- src/main.tsx
+++ src/main.tsx
@@ -4,7 +4,7 @@ import { HashRouter } from 'react-router-dom'
 import App from './App'
 import './styles.css'
 
-const APP_VERSION = 'eu-v3-personal-context'
+const APP_VERSION = 'eu-v3-cool-palette'
 
 async function refreshPwaShell() {
   if (!('serviceWorker' in navigator)) return
