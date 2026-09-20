fcd43c0c23f4bf28a2ca27aabcaa8b43f1ae7f10
caueccipriano <caueccipriano@gmail.com>
2026-09-20 14:10:00 -0300

    Bump EU UX polish version

--- src/main.tsx
+++ src/main.tsx
@@ -4,7 +4,7 @@ import { HashRouter } from 'react-router-dom'
 import App from './App'
 import './styles.css'
 
-const APP_VERSION = 'eu-v5-visual-polish'
+const APP_VERSION = 'eu-v6-ux-polish'
 
 async function refreshPwaShell() {
   if (!('serviceWorker' in navigator)) return
