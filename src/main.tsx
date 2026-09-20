1f2d2e17a3288c642e4a0624886f561226f58f50
caueccipriano <caueccipriano@gmail.com>
2026-09-20 15:09:57 -0300

    Bump EU v12 storytelling polish version

--- src/main.tsx
+++ src/main.tsx
@@ -4,7 +4,7 @@ import { HashRouter } from 'react-router-dom'
 import App from './App'
 import './styles.css'
 
-const APP_VERSION = 'eu-v11-usability-polish'
+const APP_VERSION = 'eu-v12-storytelling-polish'
 
 async function refreshPwaShell() {
   if (!('serviceWorker' in navigator)) return
