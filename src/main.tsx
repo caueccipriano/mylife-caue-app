e642a4c3c8277e5224f431395cd22f3b207be657
caueccipriano <caueccipriano@gmail.com>
2026-09-20 15:15:02 -0300

    Bump EU v14 flow safety version

--- src/main.tsx
+++ src/main.tsx
@@ -4,7 +4,7 @@ import { HashRouter } from 'react-router-dom'
 import App from './App'
 import './styles.css'
 
-const APP_VERSION = 'eu-v13-semantic-polish'
+const APP_VERSION = 'eu-v14-flow-safety'
 
 async function refreshPwaShell() {
   if (!('serviceWorker' in navigator)) return
